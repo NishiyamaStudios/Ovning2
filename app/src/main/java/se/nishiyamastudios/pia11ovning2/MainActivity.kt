6c74215f34974ec1bc227120fd37dd282bdab8b9
NishiyamaStudios <jcwasterberg@gmail.com>
2023-02-01 18:41:52 +0100

    Övning 2

---
+++ app/src/main/java/se/nishiyamastudios/pia11ovning2/MainActivity.kt
@@ -0,0 +1,11 @@
+package se.nishiyamastudios.pia11ovning2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
